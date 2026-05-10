5d29332bdc4b766d7eadc9d9313039ea9025a8ed
Josh McConkie <joshuamcconkie@gmail.com>
2026-05-09 22:14:32 -0600

    Move camera init to perception.py

--- src/ball_plate/ball_tracker_v2.py
+++ src/ball_plate/ball_tracker_v2.py
@@ -1,31 +1,8 @@
 import cv2
 
 from ball_plate import perception
-CAM_ID = 0
 
-# ===Perception/Estimation====
-def init_camera():
-    cam = cv2.VideoCapture(CAM_ID)
-    frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
-    frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    return cam
-
-
-def anchor_coordinates():
-    pass
-
-def get_ball_xy():
-    pass
-
-
-
-# ===Planning===
-def PID_control():
-    pass
-
-# ===Control===
-
-feed = init_camera()
+feed = perception.init_camera()
 ret0, frame0 = feed.read()
 filt_init_frame = perception.filter_frame(frame0)
 while True:
@@ -33,7 +10,14 @@ while True:
     if not ret:
         break
 
+    # ==Perception==
     mask = perception.get_mask(filt_init_frame, frame)
+    x,y = perception.get_position(mask)
+
+    # ==Planning==
+
+
+    #==Control==
 
 
     cv2.imshow("Webcam Feed", mask)
--- src/ball_plate/perception.py
+++ src/ball_plate/perception.py
@@ -5,6 +5,14 @@ from cv2 import COLOR_BGR2GRAY, GaussianBlur, cvtColor
 from cv2.typing import MatLike
 
 
+CAM_ID = 0
+
+def init_camera():
+    cam = cv2.VideoCapture(CAM_ID)
+    frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    return cam
+
 
 def filter_frame(frame: MatLike):
     gray = cvtColor(frame, COLOR_BGR2GRAY)
