a0031ef62f5b4b2df9264237836a437708215b2e
Josh McConkie <bjmcconk@byu.edu>
2026-07-28 09:37:19 -0600

    Simplified LinearModel class. Added child class with model update functions.

--- src/ball_plate/estimator.py
+++ src/ball_plate/estimator.py
@@ -2,6 +2,7 @@ from ball_plate.state import *
 from math import atan2, sqrt, pi
 import numpy as np
 from collections.abc import Callable
+from numpy.typing import NDArray
 
 
 def estimate_table_angle(table_old:TableState, imu_new:IMUReading):
@@ -51,44 +52,82 @@ class LinearModel:
 
     args:
 
-        A(dt): Model scalar matrix
+        get_A(dt): Model scalar matrix
             2D numpy array generator that takes dt as input
-        B(dt): Model shift
+        get_B(dt): Model shift
             2D numpy array generator that takes dt as input
-        Q(dt): Model noise covariance matrix.
+        get_Q(dt): Model noise covariance matrix.
             2D numpy array generator that takes dt as input
     '''
     def __init__(self,
-                 get_a: Callable[[float],np.ndarray],
-                 get_B: Callable[[float,float,float],np.ndarray],
-                 get_Q: Callable[[float,float,float],np.ndarray]):
-        self.get_B = get_a
-        self.get_B = get_B
-        self.get_Q = get_Q
-        self.A = None
-        self.B = None
-        self.Q = None
-
-    def init_matrices(self,A,B,Q):
-        self.A = A
-        self.B = B
-        self.Q = Q
-
-    def set_state(self,dt,roll,pitch):
-        self.A = self.A(dt)
-        self.B = self.B(dt,roll,pitch)
-        self.Q = self.Q(dt,roll,pitch)
+                 A_init: NDArray[np.float64],
+                 B_init: NDArray[np.float64],
+                 Q_init: NDArray[np.float64]):
+        self.A = A_init
+        self.B = B_init
+        self.Q = Q_init
 
 
+class LinearModel_RollPitch(LinearModel):
+    def get_tangent_scale(self, dt:float):
+        '''
+        Shifts the model prediction given table state and time passed
+        args:
+            dt: Time (ms) passed since the last estimate
+        '''
+        return np.array([[1,0,dt,0],
+                        [0,1,0,dt],
+                        [0,0,1,0],
+                        [0,0,0,1]])
+
+    def get_tangent_shift(self, dt:float, roll, pitch):
+        '''
+        Shifts the model prediction given table state and time passed
+        args:
+            dt: Time (ms) passed since the last estimate
+            roll: Table roll in degrees (cw rotation seen from x+)
+            pitch: Table pitch in degrees (cw rotation seen from y+)
+        '''
+        g = 9.8
+        c = 1 #TODO: find sphere radius constant
+        return g * c * np.array([-np.sin(2*pitch) * dt * dt,
+                                np.sin(2*roll) * dt * dt,
+                                -2 * np.sin(2*pitch) * dt,
+                                2 * np.sin(2*roll) * dt])
+
+    def get_guass_Q(self,dt:float):
+        '''
+        This Q model noise covariance assumes equal, independent, gaussian
+        acceleration noise on each axis (x,y)
+        args:
+            dt: (float) Time (ms) passed since the last estimate
+        '''
+        a = dt * dt * dt * dt / 4
+        b = dt * dt * dt / 2
+        c = dt * dt
+        acc_var = 0 # TODO: the single axis acceleration variance of the model (for Q)
+        return acc_var * np.array([[a,0,b,0],
+                                [0,a,0,b],
+                                [b,0,c,0],
+                                [0,b,0,c]])
+    
+    def update_model(self,dt: float,roll: float,pitch: float)->None:
+        '''
+        Updates the linear models coefficients given environment factors
+        args:
+            dt: Time (ms) passed since the last estimate
+            roll: Table roll in degrees (cw rotation seen from x+)
+            pitch: Table pitch in degrees (cw rotation seen from y+)
+        '''
+        self.A = self.get_tangent_scale(dt)
+        self.B = self.get_tangent_shift(dt,roll,pitch)
+        self.Q = self.get_guass_Q(dt)
 
 
 class KalmanFilter:
     def __init__(self,model,P_init:np.ndarray):
         self.model = model
-
-        self.P_prev = P_init
-        self.P = P_init
-        self.P_minus = P_init
+        self.P_prev = self.P = self.P_minus = P_init # initial estimate noise
 
         self.H = np.array([[1,0,0,0],
                            [0,1,0,0]])
@@ -97,16 +136,16 @@ class KalmanFilter:
         self.state_est_minus = np.ndarray((4,1)) # x,y,vx,vy
         self.state_est = np.ndarray((4,1)) # x,y,vx,vy
 
-    def h_predict(self,dt,phi,theta):
+    def h_predict(self,dt,roll,pitch):
         '''
         Estimate the current state using the model and table params
         '''
-        self.model.set_state(dt,phi,theta)
+        self.model.set_state(dt,roll,pitch)
         self.state_est_minus = self.model.A @ self.state_est_prev + self.model.B
         self.P_minus = self.model.A @ self.model.P @ self.model.A.T + self.model.Q
         return self.state_est_minus
 
-    def h_update(self,meas: np.ndarray[float], meas_cov):
+    def h_update(self,meas: NDArray[np.float64], meas_cov):
         '''
         Update the estimate with a measurement
         '''
@@ -116,15 +155,18 @@ class KalmanFilter:
         self.P = (np.identity(self.P.shape[0]) - K @ self.H) @ self.P_minus
         return self.state_est
 
-    def estimate_state(self,dt,meas,meas_cov):
-        self.h_predict(dt)
+    def estimate_state(self,dt,roll,pitch,meas,meas_cov):
+        self.h_predict(dt,roll,pitch)
         return self.h_update(meas, meas_cov)
 
+# get_A()
 def get_tangent_step(dt:float):
     return np.array([[1,0,dt,0],
                      [0,1,0,dt],
                      [0,0,1,0],
                      [0,0,0,1]])
+
+#  get_B()
 def get_tangent_shift(dt:float,roll,pitch):
     g = 9.8
     c = 1 #TODO: find sphere radius constant
@@ -149,7 +191,7 @@ def get_tangent_Q_cov(dt:float,roll,pitch):
                                [0,b,0,c]])
 
 # Initial matrix construction
-def measure_camera_mm_covariance()->np.ndarray[float]:
+def measure_camera_mm_covariance()->NDArray[np.float64]:
     # TODO: measure still camera noise (R)
     pass
 
