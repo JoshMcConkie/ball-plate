586a8d80deeecca40c953efa9545cba30a1dc13d
Josh McConkie <joshuamcconkie@gmail.com>
2026-08-12 12:13:45 -0600

    IMU Reader class implemented

--- src/ball_plate/perception/imu.py
+++ src/ball_plate/perception/imu.py
@@ -1,8 +1,17 @@
 
+from time import time
+
 from serial import Serial
 from ball_plate import serial_io
 from ball_plate.state import IMUMeasurement
 
+class IMUReader:
+    def __init__(self, ser: Serial):
+        self.ser = ser
 
-def measure(ser: Serial)->IMUMeasurement:
-    return serial_io.fetch_packet(ser)
+    def measure(self)->IMUMeasurement | None:
+        values = serial_io.fetch_values(self.ser)
+        if values is None:
+            return None
+        return IMUMeasurement(time.monotonic(), *values)
+    
